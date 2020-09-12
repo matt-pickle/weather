1d1f1b037e8dde84b35c53ff6782ef586f03ad18
Matt <mpickle86@gmail.com>
2020-09-12 17:12:38 -0500

    Set up current weather output

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Current from "./Current";
 
 function App() {
+  const [weatherObj, setWeatherObj] = useState();
+
+  useEffect(() => {
+    //Get latitude and longitude coordinates from zip code
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=73008&appid=${process.env.REACT_APP_WEATHER_KEY}`)
+    .then(res => {
+      if (res.ok) {
+        res.json().then(data => {
+          //Get weather data from coordinates
+          fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`)
+          .then(res => {
+            if (res.ok) {
+              res.json().then(data => setWeatherObj(data));
+            } else {
+              console.error("API Request Failed!");
+            }
+          })
+        })  
+      } else {
+        console.error("API Request Failed!");
+      }
+    })
+  },[])
+ 
+
   return (
     <div className="app">
-      <h1>Hello, {process.env.REACT_APP_SECRET_CODE}</h1>
+      <h1>Hello world</h1>
+      {weatherObj ? <Current weatherObj={weatherObj}/> : "Loading..."}      
     </div>
   );
 }
---
+++ src/components/Current.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+
+function Current(props) { 
+  const current = props.weatherObj.current;
+  const [description, setDescription] = useState("");
+  
+  useEffect(() => {
+    //Creates weather description string
+    current.weather.forEach((item, index) => {
+      if (index === 0) {
+        setDescription(item.description);
+      } else {
+        setDescription(description + ", " + item.description);
+      }
+    });
+  }, []);
+  
+  return (
+    <div className="current-container">
+      <h1>Current Weather</h1>
+      <p>Current Temp: {Math.round(current.temp)}&deg;F</p>
+      <p>Conditions: {description}</p>
+      <p>Feels Like: {Math.round(current.feels_like)}&deg;F</p>
+      <p>Humidity: {current.humidity}%</p>
+      <p>Wind Speed: {Math.round(current.wind_speed)}mph</p>
+    </div>
+  );
+}
+
+export default Current;
