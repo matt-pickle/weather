bfb67baad08880178f77ca94c714ee09f6b52e92
Matt <mpickle86@gmail.com>
2020-09-15 18:53:14 -0500

    Add night time check for background

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,23 +4,28 @@ function Background(props) {
   const [background, setBackground] = useState("");
 
   useEffect(() => {
-    let conditionArr = [];
-
-    //Creates array of current weather conditions
     if (props.weatherObj) {
-      props.weatherObj.current.weather.map(item => {
-        conditionArr.push(item.main, item.id);
-      });
-    }
-
-    //Selects background image based on current weather conditions
-    conditionArr.includes("Snow") ? setBackground("snow")
-    : conditionArr.includes("Thunderstorm" || "Squall" || "Torndado") ? setBackground("thunderstorm")
-    : conditionArr.includes("Drizzle" || "Rain") ? setBackground("rain")
-    : conditionArr.includes(802 || 803) ? setBackground("partly-cloudy")
-    : conditionArr.includes(804 || "Fog") ? setBackground("cloudy")
-    : conditionArr.includes("Clear" || 801) ? setBackground("sunny")
-    : setBackground("");
+      const current = props.weatherObj.current;
+      //Checks for night time
+      const now = new Date() * 1000;
+      if (now < current.sunrise || now === current.sunset) {
+        setBackground("night");
+      } else {
+        //Creates array of current weather conditions
+        let conditionArr = [];
+        current.weather.map(item => {
+          conditionArr.push(item.main, item.id);
+        });
+        //Selects background image based on current weather conditions
+        conditionArr.includes("Snow") ? setBackground("snow")
+        : conditionArr.includes("Thunderstorm" || "Squall" || "Torndado") ? setBackground("thunderstorm")
+        : conditionArr.includes("Drizzle" || "Rain") ? setBackground("rain")
+        : conditionArr.includes(802 || 803) ? setBackground("partly-cloudy")
+        : conditionArr.includes(804 || "Fog") ? setBackground("cloudy")
+        : conditionArr.includes("Clear" || 801) ? setBackground("sunny")
+        : setBackground("");
+      }
+    } 
   }, [props.weatherObj]);
   
   return (
