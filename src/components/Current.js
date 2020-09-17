0915daf50e79e5ee2f1f2f97aa01f05fe2461657
Matt <mpickle86@gmail.com>
2020-09-17 18:37:46 -0500

    Set up current weather icon

--- src/components/Current.js
+++ src/components/Current.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 function Current(props) { 
   const current = props.weatherObj.current;
   const [description, setDescription] = useState("");
+  const [icon, setIcon] = useState("");
   
   useEffect(() => {
     //Creates weather description string
@@ -13,11 +14,31 @@ function Current(props) {
         setDescription(description + ", " + item.description);
       }
     });
+    //Creates array of current weather conditions
+    let conditionArr = [];
+    current.weather.map(item => {
+      conditionArr.push(item.main, item.id);
+    });
+    //Checks for night time
+    const now = new Date() / 1000;
+    if (now < current.sunrise || now > current.sunset) {
+      conditionArr.push("Night");
+    }
+    //Selects icon based on current weather conditions
+    conditionArr.includes("Snow") ? setIcon("snow.png")
+    : conditionArr.includes("Thunderstorm" || "Squall" || "Tornado") ? setIcon("thunderstorm.png")
+    : conditionArr.includes("Drizzle" || "Rain") ? setIcon("rain.png")
+    : conditionArr.includes("Night") ? setIcon("night.png")
+    : conditionArr.includes(802 || 803) ? setIcon("partly-cloudy.png")
+    : conditionArr.includes(804 || "Fog") ? setIcon("cloudy.png")
+    : conditionArr.includes("Clear" || 801) ? setIcon("sunny.png")
+    : setIcon("");
   }, []);
   
   return (
     <div className="current-container">
       <h1>Current Weather</h1>
+      <img src={icon} alt="weather icon" className="current-icon" />
       <p>Current Temp: {Math.round(current.temp)}&deg;F</p>
       <p>Conditions: {description}</p>
       <p>Feels Like: {Math.round(current.feels_like)}&deg;F</p>
