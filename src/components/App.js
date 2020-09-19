a03ee405f6024643e9736fdd46c7e5c3f82e6bc8
Matt <mpickle86@gmail.com>
2020-09-18 19:02:57 -0500

    Lift currentWeather state and delete Background component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import Background from "./Background";
 import Input from "./Input";
 import Current from "./Current";
 import Hourly from "./Hourly";
@@ -7,6 +6,7 @@ import Daily from "./Daily";
 
 function App() {
   const [weatherObj, setWeatherObj] = useState();
+  const [currentWeather, setCurrentWeather] = useState("");
 
   function handleZipSubmit(zip) {
     //Get latitude and longitude coordinates from zip code
@@ -18,7 +18,10 @@ function App() {
           fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`)
           .then(res => {
             if (res.ok) {
-              res.json().then(data => setWeatherObj(data));
+              res.json().then(data => {
+                setWeatherObj(data);
+                getCurrentWeather(data);
+              })
             } else {
               console.error("API Request Failed!");
             }
@@ -30,14 +33,50 @@ function App() {
     })
   }
 
+  function getCurrentWeather(data) {
+    //Creates array of current weather conditions
+    const current = data.current;
+    let conditionArr = [];
+    current.weather.map(item => {
+      conditionArr.push(item.main, item.id);
+    });
+    //Checks for night time
+    const now = new Date() / 1000;
+    if (now < current.sunrise || now > current.sunset) {
+      conditionArr.push("Night");
+    }
+    //Selects icon/background based on current weather conditions
+    conditionArr.includes("Snow") ? setCurrentWeather("snow")
+    : conditionArr.some(el => el === "Thunderstorm" ||
+                              el === "Squall" ||
+                              el === "Tornado") ?
+                              setCurrentWeather("thunderstorm")
+    : conditionArr.some(el => el === "Drizzle" || el === "Rain") ? setCurrentWeather("rain")
+    : conditionArr.includes("Night") ? setCurrentWeather("night")
+    : conditionArr.some(el => el === 802 || el === 803) ? setCurrentWeather("partly-cloudy")
+    : conditionArr.some(el => el === 804 ||
+                              el === "Fog" ||
+                              el === "Smoke" ||
+                              el === "Mist" ||
+                              el === "Haze" ||
+                              el === "Dust" ||
+                              el === "Sand" ||
+                              el === "Ash") ?
+                              setCurrentWeather("cloudy")
+    : conditionArr.some(el => el === "Clear" || el === 801) ? setCurrentWeather("sunny")
+    : setCurrentWeather("");
+  }
+
   return (
     <div className="app">
-      <Background weatherObj={weatherObj} />
+      <div className={"background " + currentWeather}></div>
       <Input handleZipSubmit={handleZipSubmit} />
       {
       weatherObj ?
         <div className="output-container">
-          <Current weatherObj={weatherObj} />
+          <Current weatherObj={weatherObj}
+                   currentWeather={currentWeather}
+          />
           <Hourly weatherObj={weatherObj} />
           <Daily weatherObj={weatherObj} />
         </div>
--- src/components/Current.js
+++ src/components/Current.js
@@ -3,7 +3,6 @@ import React, {useState, useEffect} from 'react';
 function Current(props) { 
   const current = props.weatherObj.current;
   const [description, setDescription] = useState("");
-  const [icon, setIcon] = useState("");
   
   useEffect(() => {
     //Creates weather description string
@@ -14,31 +13,12 @@ function Current(props) {
         setDescription(description + ", " + item.description);
       }
     });
-    //Creates array of current weather conditions
-    let conditionArr = [];
-    current.weather.map(item => {
-      conditionArr.push(item.main, item.id);
-    });
-    //Checks for night time
-    const now = new Date() / 1000;
-    if (now < current.sunrise || now > current.sunset) {
-      conditionArr.push("Night");
-    }
-    //Selects icon based on current weather conditions
-    conditionArr.includes("Snow") ? setIcon("snow.png")
-    : conditionArr.some(el => el === "Thunderstorm" || el === "Squall" || el === "Tornado") ? setIcon("thunderstorm.png")
-    : conditionArr.some(el => el === "Drizzle" || el === "Rain") ? setIcon("rain.png")
-    : conditionArr.includes("Night") ? setIcon("night.png")
-    : conditionArr.some(el => el === 802 || el === 803) ? setIcon("partly-cloudy.png")
-    : conditionArr.some(el => el === 804 || el === "Fog") ? setIcon("cloudy.png")
-    : conditionArr.some(el => el === "Clear" || el === 801) ? setIcon("sunny.png")
-    : setIcon("");
   }, []);
   
   return (
     <div className="current-container">
       <h1>Current Weather</h1>
-      <img src={icon} alt="weather icon" className="current-icon" />
+      <img src={props.currentWeather + ".png"} alt="weather icon" className="current-icon" />
       <p>Current Temp: {Math.round(current.temp)}&deg;F</p>
       <p>Conditions: {description}</p>
       <p>Feels Like: {Math.round(current.feels_like)}&deg;F</p>
